1f77eb6b8cf5b00139e6a9f8a17c4a9e13e4f014
Fallendusk89 <121059396+Fallendusk89@users.noreply.github.com>
2022-12-22 13:34:07 -0700

    Update santa.gd

--- santa.gd
+++ santa.gd
@@ -7,7 +7,7 @@ var armor = 0.0
 
 func damage_santa(amount):
 	if health - amount <= 0:
-		get_tree().reload_current_scene()
+		get_tree().change_scene("res://DeathScreen.tscn")
 		return
 	
 	health -= (amount * (1 - (armor / 100)))
